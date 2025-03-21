16f2995e6bcf6a856d20cbf5ae6ccecce196fe3e
Giovanni-10-96 <giovanni.barbetti10@gmail.com>
2025-03-21 02:20:11 +0100

    Creare la Searchbar e la ricerca avanzata

--- lib/ui/pages/my_courses_page.dart
+++ lib/ui/pages/my_courses_page.dart
@@ -3,19 +3,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyScaffold extends StatefulWidget {
+const List<String> _typeCourse = <String>['ACQUAGYM', 'CROSSFIT', 'PILATES', 'ZUMBA_FITNESS', 'POSTURAL_TRAINING', 'BODYWEIGHT_WORKOUT'];
+
+class MyCoursesPage extends StatefulWidget {
 
   @override
-  State<MyScaffold> createState() => _MyScaffoldState();
+  State<MyCoursesPage> createState() => _MyScaffoldState();
 }
 
-class _MyScaffoldState extends State<MyScaffold> {
+class _MyScaffoldState extends State<MyCoursesPage> {
+
+  String _dropDownValue = _typeCourse.first;
+
+  bool _showAdvancedSearch = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pagina del profilo"),
+        title: Text("Corsi a cui ti sei iscritto"),
         backgroundColor: Colors.blue,
       ),
 
@@ -25,26 +31,81 @@ class _MyScaffoldState extends State<MyScaffold> {
         //I vari componenti della LISTA "ListView" si chiamano "ListTile"
         children: [
 
+          SearchAnchor(
+              builder: (BuildContext context, SearchController controller){
+                return SearchBar(
+                  controller: controller,
+
+                  onTap: (){
+                    controller.openView();
+                  },
+
+                  trailing: [
+                    IconButton(
+                        onPressed:() {},
+                        icon: const Icon(Icons.search),
+
+                    )
+                  ],
+                );
+              },
+
+              suggestionsBuilder: (BuildContext context, SearchController controller){
+                return List<ListTile>.generate(5, (int index){
+                  final String corso = 'corso $index';
+                  return ListTile(
+                    title: Text(corso),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(corso);
+                      });
+                    },
+                  );
+                });
+              }
+
+          ),
+
+          ElevatedButton(
+              child: const Text('Ricerca avanzata'),
+              onPressed: () {
+                setState(() {
+                  _showAdvancedSearch = !_showAdvancedSearch;
+                });
+              }
+
+          ),
+
+
+          Offstage(   //Ho usato "Offstage" invece di "Visibility", perchè la seconda lascia lo SPAZIO BIANCO (rende invisibile l'oggetto, ma continua ad occupare spazio), mentre la prima NO (Sparisce senza occupare spazio bianco)
+            offstage: _showAdvancedSearch,
+            child: DropdownButton(
+                value: _dropDownValue,
+                icon: const Icon(Icons.arrow_downward),
+
+                onChanged: (String? value){
+                  setState(() {
+                      _dropDownValue = value!;
+                  });
+                },
+                items:
+                  _typeCourse.map<DropdownMenuItem<String>>((String value){
+                    return DropdownMenuItem<String>(value: value, child: Text(value));
+                  }).toList(),
+
+            )
+          ),
+
+          Divider(),
+
           Card(
-            child: ListTile(
-                title: Image.asset("images/blankProfileImage.png")
-            ),
-
-          ),
-
-          Card(   //Card permette di rendere più bello graficamente l'elemento della lista (crea un riquadro con bordi smussati)
             child:ListTile(
-                title: Text("Nome"),
-                subtitle: Text("Marco"),
-                trailing:  Icon(Icons.mode_edit)
-            ),
-          ),
-
-          Card(   //Card permette di rendere più bello graficamente l'elemento della lista (crea un riquadro con bordi smussati)
-            child:ListTile(
-              title: Text("Cognome"),
-              subtitle: Text("Polo"),
-              trailing: Icon(Icons.mode_edit),    //trailing = Aggiungiamo a DESTRA qualcosa (in questo caso un'icona)
+              title: Text("Corso 1"),
+              subtitle: Text("Attivo"),
+              trailing: ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('Dettagli')
+              ),
             ),
           ),
 
