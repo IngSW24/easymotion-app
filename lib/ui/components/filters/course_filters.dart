d92ad10812a586994979d4ab33b2c580c7d7a33f
Nicola Revelant <nicolarevelant@outlook.com>
2025-03-22 22:46:15 +0100

    Explore page: filter by categoria, livello, frequenza, disponibilità

--- lib/ui/components/filters/course_filters.dart
+++ lib/ui/components/filters/course_filters.dart
@@ -4,24 +4,30 @@ import 'package:go_router/go_router.dart';
 class CourseFilter extends StatelessWidget {
   const CourseFilter(
       {super.key,
-      required this.startDate,
-      required this.endDate,
-      required this.onStartDateChange,
-        required this.onEndDateChange});
+      required this.selectedCategories,
+        required this.selectedLevels,
+        required this.selectedFrequencies,
+        required this.selectedAvailabilities,
+      required this.onCategoriesChanged,
+        required this.onLevelsChanged,
+        required this.onFrequenciesChanged,
+        required this.onAvailabilitiesChanged});
 
-  final DateTime startDate, endDate;
-  final void Function(DateTime) onStartDateChange;
-  final void Function(DateTime) onEndDateChange;
+  final List<String> selectedCategories, selectedLevels, selectedFrequencies, selectedAvailabilities;
+  final void Function(List<String>) onCategoriesChanged, onLevelsChanged, onFrequenciesChanged, onAvailabilitiesChanged;
 
-  Future<void> _askStartDate(BuildContext context) async {
-    var date = await showDatePicker(context: context, firstDate: DateTime.timestamp(), lastDate: DateTime.timestamp());
-    if (date != null) onStartDateChange(date);
-  }
-
-  Future<void> _askEndDate(BuildContext context) async {
-    var date = await showDatePicker(context: context, firstDate: DateTime.timestamp(), lastDate: DateTime.timestamp());
-    if (date != null) onEndDateChange(date);
-  }
+  static const List<String> categories = [
+    "Acquagym", "Crossfit"
+  ];
+  static const List<String> levels = [
+    "Base", "Medio", "Avanzato"
+  ];
+  static const List<String> frequencies = [
+    "Sessione singola", "Settimanale", "Mensile"
+  ];
+  static const List<String> availabilities = [
+    "Attivo", "Disponibilità a breve", "Non disponibile"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,24 +35,79 @@ class CourseFilter extends StatelessWidget {
     return Container(
       padding: EdgeInsets.all(16),
       height: 300,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: ListView(
+        //crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.only(bottom: 16),
             child: Text('Select filters',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           ),
-          OutlinedButton(onPressed: () => _askStartDate(context), child: Text("Select start date")),
-          OutlinedButton(onPressed: () => _askEndDate(context), child: Text("Select end date")),
+          Text("Categorie"),
+          Column(children:
+          categories.map((value) {
+              return CheckboxListTile(
+                title: Text(value),
+                value: selectedCategories.contains(value),
+                onChanged: (bool? selected) {
+                  if (selected != null && selected) {
+                    onCategoriesChanged(selectedCategories + [value]);
+                  } else {
+                    selectedCategories.remove(value);
+                    onCategoriesChanged(selectedCategories);
+                  }
+              });
+          }).toList()),
+          Text("Livelli"),
+          Column(children:
+          levels.map((value) {
+            return CheckboxListTile(
+                title: Text(value),
+                value: selectedLevels.contains(value),
+                onChanged: (bool? selected) {
+                  if (selected != null && selected) {
+                    onLevelsChanged(selectedLevels + [value]);
+                  } else {
+                    selectedLevels.remove(value);
+                    onLevelsChanged(selectedLevels);
+                  }
+                });
+          }).toList()),
+          Text("Frequenze"),
+          Column(children:
+          frequencies.map((value) {
+            return CheckboxListTile(
+                title: Text(value),
+                value: selectedFrequencies.contains(value),
+                onChanged: (bool? selected) {
+                  if (selected != null && selected) {
+                    onFrequenciesChanged(selectedFrequencies + [value]);
+                  } else {
+                    selectedFrequencies.remove(value);
+                    onFrequenciesChanged(selectedFrequencies);
+                  }
+                });
+          }).toList()),
+          Text("Disponibilità"),
+          Column(children:
+          availabilities.map((value) {
+            return CheckboxListTile(
+                title: Text(value),
+                value: selectedAvailabilities.contains(value),
+                onChanged: (bool? selected) {
+                  if (selected != null && selected) {
+                    onAvailabilitiesChanged(selectedAvailabilities + [value]);
+                  } else {
+                    selectedAvailabilities.remove(value);
+                    onAvailabilitiesChanged(selectedAvailabilities);
+                  }
+                });
+          }).toList()),
           Spacer(),
-          Expanded(
-            flex: 0,
-            child: FilledButton.icon(
-              onPressed: () => context.pop(),
-              label: Text('Apply Filters'),
-              icon: Icon(Icons.check),
-            ),
+          FilledButton.icon(
+            onPressed: () => context.pop(),
+            label: Text('Apply Filters'),
+            icon: Icon(Icons.check),
           ),
         ],
       ),
