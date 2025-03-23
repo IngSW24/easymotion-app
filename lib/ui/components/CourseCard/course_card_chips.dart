0906802ace8baee82f07b0fee04b32737b061c59
Nicola Revelant <nicolarevelant@outlook.com>
2025-03-23 18:38:10 +0100

    Apply course filters and removed search history

---
+++ lib/ui/components/CourseCard/course_card_chips.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CourseCardChips extends StatelessWidget {
+  const CourseCardChips({
+    super.key,
+    required this.courseTags,
+  });
+
+  final List<String> courseTags;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: courseTags.length,
+      itemBuilder: (BuildContext ctx, int index) => SizedBox(
+        width: 100,
+        child: Chip(
+            label: Text(
+              courseTags[index],
+              overflow: TextOverflow.ellipsis,
+            ),
+            side: BorderSide.none,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            backgroundColor: Color(0xffcae2fc)),
+      ),
+      separatorBuilder: (BuildContext context, int index) =>
+          SizedBox(width: 10),
+    );
+  }
+}
