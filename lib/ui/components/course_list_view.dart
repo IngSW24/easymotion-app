c4b68c66ca0d5bda363c5665e441f68a1c120a31
Nicola Revelant <nicolarevelant@outlook.com>
2025-03-23 18:38:10 +0100

    Apply course filters and removed search history

--- lib/ui/components/course_list_view.dart
+++ lib/ui/components/course_list_view.dart
@@ -1,11 +1,16 @@
-//import 'package:easymotion_app/api-client-generated/lib/api.dart';
+import 'package:easymotion_app/api-client-generated/schema.models.swagger.dart';
 import 'package:easymotion_app/data/hooks/use_courses.dart';
-import 'package:easymotion_app/ui/components/course_card.dart';
+import 'package:easymotion_app/ui/components/CourseCard/course_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 
 class CourseListView extends HookWidget {
   const CourseListView({super.key});
+
+  void onCourseClick(CourseEntity courseEntity, BuildContext context) {
+    context.go("/details");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +33,12 @@ class CourseListView extends HookWidget {
     return GridView.builder(
       itemCount: courseList.length,
       itemBuilder: (BuildContext ctx, int index) {
-        return CourseCard(course: courseList[index]);
+        return Padding(
+            padding: EdgeInsets.all(6),
+            child: CourseCard(
+              course: courseList[index],
+              onClick: () => onCourseClick(courseList[index], ctx),
+            ));
       },
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 600,
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,17 +2,18 @@ import 'package:easymotion_app/ui/components/course_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key /*, required this.coursesApi*/});
-
-  //final CoursesApi coursesApi;
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('App title'),
-      ),
-      body: CourseListView(/*coursesApi: coursesApi*/),
-    );
+        appBar: AppBar(
+          title: Text('Easymotion'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: CourseListView());
   }
 }
