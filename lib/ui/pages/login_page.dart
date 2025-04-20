5f95cfff287dc916866dde73142397377e6e566f
Nicola Revelant <nicolarevelant@outlook.com>
2025-04-20 17:00:25 +0200

    login page: add register button

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -3,6 +3,9 @@ import 'package:easymotion_app/data/hooks/use_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../data/common/constants.dart';
 
 class LoginPage extends StatefulHookWidget {
   const LoginPage({super.key});
@@ -14,6 +17,19 @@ class LoginPage extends StatefulHookWidget {
 class _LoginPageState extends State<LoginPage> {
   String _username = "", _password = "";
   bool loginFailed = false;
+
+  Future<void> onRegisterClick() async {
+    final Uri uri = Uri.parse(registerUrl!);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Impossibile aprire l\'URL: $uri')),
+      );
+    } else {
+      debugPrint("Impossibile aprire l\'URL: $uri");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +89,12 @@ class _LoginPageState extends State<LoginPage> {
                       }
                     },
                     child: Text("Login"))),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: TextButton(
+                  onPressed: onRegisterClick,
+                  child: Text("Registati a Easymotion")),
+            )
           ],
         ));
   }
