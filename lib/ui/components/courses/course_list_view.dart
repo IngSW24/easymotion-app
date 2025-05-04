2350724ddcc1b436b8cde9dc75e5a283a0074267
Nicola Revelant <nicolarevelant@outlook.com>
2025-05-04 17:23:23 +0200

    refactor: use CourseCard and an adaptive GridView

--- lib/ui/components/courses/course_list_view.dart
+++ lib/ui/components/courses/course_list_view.dart
@@ -1,13 +1,11 @@
 import 'package:easymotion_app/api-client-generated/api_schema.models.swagger.dart';
 import 'package:easymotion_app/data/hooks/use_courses.dart';
+import 'package:easymotion_app/ui/components/courses/course_card.dart';
 import 'package:easymotion_app/ui/components/utility/empty_alert.dart';
 import 'package:easymotion_app/ui/components/utility/error_alert.dart';
 import 'package:easymotion_app/ui/components/utility/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
-
-import '../../../data/common/static_resources.dart';
 import 'course_filter.type.dart';
 
 class CourseListView extends HookWidget {
@@ -54,82 +52,16 @@ class CourseListView extends HookWidget {
       return EmptyAlert();
     }
 
-    return GridView.count(
-      crossAxisCount: 1,
-      mainAxisSpacing: 25,
-      children: List.generate(courseList.length, (index) {
-        return Card(
-            child: InkWell(
-          onTap: () => context.push('/details/${courseList[index].id}'),
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Image.network(
-                    "${StaticResources.uri}/${courseList[index].category.id}.jpg"),
-              ),
-              Center(
-                child: Text(
-                  "\n${courseList[index].name}",
-                  style: TextStyle(
-                      color: Color(0xFF094D95), fontWeight: FontWeight.bold),
-                ),
-              ),
-              Text(courseList[index].shortDescription),
-
-              /*
-                          ElevatedButton(
-
-                            style: ButtonStyle(
-                              backgroundColor: WidgetStatePropertyAll(Color(0xFF094D95))
-                            ),
-                            onPressed: () =>
-                                context.go('/explore/details/${courseList[index].id}'),
-
-                            child: Row(
-                              children: [
-                                Icon(Icons.launch, color: Color(0xFFFDFDFD)),
-                                Center(
-                                  child: Text("  Dettagli corso", style: TextStyle(color: Color(0xFFFDFDFD), fontWeight: FontWeight.bold)),
-                                )
-
-                              ],
-                            ),
-                          )
-                          */
-            ]),
-          ),
-        ));
-      }),
-    );
-
-    /*return ListView.builder(
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 340,
+          childAspectRatio: 3 / 5,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8),
       itemCount: courseList.length,
-      itemBuilder: (BuildContext ctx, int index) {
-        return ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          title: Text(
-            courseList[index].name,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(color: Color(0xFF094D95), fontWeight: FontWeight.bold),
-          ),
-          subtitle: Text(
-            courseList[index].shortDescription,
-          ),
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(
-              "${StaticResources.uri}/${courseList[index].category.id}.jpg",
-            ),
-          ),
-          trailing: IconButton(
-            tooltip: "Dettagli corso",
-            onPressed: () => context.push('/details/${courseList[index].id}'),
-            icon: Icon(Icons.launch),
-          ),
-        );
+      itemBuilder: (context, index) {
+        return CourseCard(course: courseList[index]);
       },
-    );*/
+    );
   }
 }
